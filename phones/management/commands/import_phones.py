9d18002ae4fee72a4793a908a654179f9593b3c6
OlegKumachev <117755901+OlegKumachev@users.noreply.github.com>
2023-06-03 18:44:44 +0300

    Update import_phones.py

--- phones/management/commands/import_phones.py
+++ phones/management/commands/import_phones.py
@@ -1,12 +1,11 @@
 import csv
-
+from datetime import datetime
+from django.utils.text import slugify
 from django.core.management.base import BaseCommand
 from phones.models import Phone
 
-
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        pass
+    parser.add_argument('phones.csv', type=str)
 
     def handle(self, *args, **options):
         with open('phones.csv', 'r') as csvfile:
@@ -16,5 +15,21 @@ class Command(BaseCommand):
             next(phone_reader)
 
             for line in phone_reader:
-                # TODO: Добавьте сохранение модели
-                pass
+                id = int(line[0])
+                name = line[1]
+                price = line[3]
+                image = line[2]
+                release_date = datetime.strptime(line[4], '%Y-%m-%d').date()
+                lte_exists = bool(line[5])
+                slug = slugify(name)
+
+                phone = Phone(
+                    id=id,
+                    name=name,
+                    price=price,
+                    image=image,
+                    release_date=release_date,
+                    lte_exists=lte_exists,
+                    slug=slug,
+                )
+                phone.save()
